d3f2dd407a838be05d7b4a99c81fb038353973d2
farmology <abkim03@gmail.com>
2022-06-13 00:24:13 -0700

    Try to upload to main branch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
             </Toolbar>
             <Box display="flex">
                 <Typography variant="h6" className={classes.title}>
-                    Explore new places!
+                    Explore new places...
                 </Typography>
                 {/* <Autocomplete> */}
                     <div className={classes.search}>
